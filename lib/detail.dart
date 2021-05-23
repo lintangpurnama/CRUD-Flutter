f5d35ec587c3fb67aea99cbb3210aa0bbf211a71
lintang purnama <lintangpurnama1@gmail.com>
2021-05-24 04:12:59 +0700

    Tugas 1

---
+++ lib/detail.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import './edit.dart';
+import 'package:http/http.dart' as http;
+import './main.dart';
+
+class Detail extends StatefulWidget {
+  List list;
+  int index;
+  Detail({this.index, this.list});
+
+  @override
+  _DetailState createState() => new _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  void deleteData() {
+    var url = "http://10.237.89.232/my_store/delete.php";
+    http.post(url, body: {'id': widget.list[widget.index]['id']});
+  }
+
+  void confirm() {
+    AlertDialog alertDialog = new AlertDialog(
+      content: new Text(
+          "Are You sure want to delete ?'${widget.list[widget.index]['item_name']}'"),
+      actions: <Widget>[
+        new RaisedButton(
+          child: new Text(
+            "OK DELETE !",
+            style: new TextStyle(color: Colors.black),
+          ),
+          color: Colors.red,
+          onPressed: () {
+            deleteData();
+            Navigator.of(context).push(new MaterialPageRoute(
+              builder: (BuildContext context) => new Home(),
+            ));
+          },
+        ),
+        new RaisedButton(
+          child: new Text(
+            "CANCEL !",
+            style: new TextStyle(color: Colors.black),
+          ),
+          color: Colors.green,
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
+    );
+
+    showDialog(builder: (context) => alertDialog, context: context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+            title: new Text("${widget.list[widget.index]['item_name']}")),
+        body: new Container(
+          height: 250.0,
+          padding: const EdgeInsets.all(20.0),
+          child: new Card(
+            child: new Center(
+              child: new Column(
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                  ),
+                  new Text(
+                    widget.list[widget.index]['item_name'],
+                    style: new TextStyle(fontSize: 20.0),
+                  ),
+                  new Text(
+                    "Code : ${widget.list[widget.index]['item_code']}",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                  new Text(
+                    "Price : ${widget.list[widget.index]['price']}",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                  new Text(
+                    "Stock : ${widget.list[widget.index]['stock']}",
+                    style: new TextStyle(fontSize: 18.0),
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 30.0),
+                  ),
+                  new Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      new RaisedButton(
+                          child: new Text("EDIT"),
+                          color: Colors.green,
+                          onPressed: () =>
+                              Navigator.of(context).push(new MaterialPageRoute(
+                                builder: (BuildContext context) => new EditData(
+                                  list: widget.list,
+                                  index: widget.index,
+                                ),
+                              ))),
+                      new RaisedButton(
+                        child: new Text("DELETE"),
+                        color: Colors.red,
+                        onPressed: () => confirm(),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
